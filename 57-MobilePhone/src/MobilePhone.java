3e1eb4ef0546d6ae89e529b646d62592e0699254
gsajbura <gsajbura@LAW250.dps-ad.pl>
2018-01-29 22:57:44 +0100

    Mobile Phone simulation

--- 57-MobilePhone/src/MobilePhone.java
+++ 57-MobilePhone/src/MobilePhone.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 
 public class MobilePhone {
 
-	Scanner scanner = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
 	private static ArrayList<Contact> contacts = new ArrayList<Contact>();
 
 	public void useMobielPhone() {
 		boolean quit = false;
 		printOptionList();
 		while (!quit) {
-			System.out.print("Choose option: ");
+			System.out.print("\nChoose option: ");
 			int choice = scanner.nextInt();
+			scanner.nextLine();
 			switch (choice) {
 			case 0:
 				printOptionList();
@@ -20,19 +21,33 @@ public class MobilePhone {
 				printContacts();
 				break;
 			case 2:
+				System.out.println("ADD CONTACT");
 				addContact();
+				break;
+			case 3:
+				System.out.println("MODIFY CONTACT");
+				modifyContact();
+				break;
+			case 4:
+				System.out.println("REMOVE CONTACT");
+				removeContact();
+				break;
+			case 5:
+				System.out.println("SEARCH");
+				searchForContact();
 				break;
 			case 9:
 				quit = true;
 				break;
 			default:
+				System.out.println("Wrong option number");
 				break;
 			}
 		}
 	}
 
 	private void printOptionList() {
-		System.out.println("\nPress");
+		System.out.println("Press number");
 		System.out.println("\t0 - Print options");
 		System.out.println("\t1 - Print list of contacts");
 		System.out.println("\t2 - Add new contact");
@@ -43,13 +58,12 @@ public class MobilePhone {
 	}
 
 	private void addContact() {
-		Scanner input = new Scanner(System.in);
 		System.out.print("Type name: ");
-		String name = input.nextLine();
+		String name = scanner.nextLine();
 		System.out.print("Type number: ");
-		String number = input.nextLine();
+		String number = scanner.nextLine();
 		contacts.add(new Contact(name, number));
-		System.out.println("New contact added");
+		System.out.println("New contact " + name + " added with number " + number);
 	}
 
 	private void printContacts() {
@@ -59,6 +73,61 @@ public class MobilePhone {
 			System.out.println(i + ". " + x.getName() + " " + x.getNumber());
 			i++;
 		}
+	}
+
+	private void modifyContact() {
+		int position = findContact();
+		if (position >= 0) {
+			System.out.println("Type new name: ");
+			String name = scanner.nextLine();
+			System.out.println("Type new number: ");
+			String number = scanner.nextLine();
+			contacts.set(position, new Contact(name, number));
+			System.out.println("Contact " + name + " modified, phone number: " + number);
+		}
+	}
+
+	private void removeContact() {
+		int position = findContact();
+		if (position >= 0) {
+			System.out.print("Do you want to remove this contact? 1-YES, 0-NO: ");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				contacts.remove(position);
+				System.out.println("Contact removed");
+				break;
+			case 0:
+				System.out.println("Aborted");
+				break;
+			default:
+				System.out.println("Wrong option number");
+				break;
+			}
+		}
+
+	}
+
+	private int findContact() {
+		System.out.print("Type name of a contact: ");
+		String name = scanner.nextLine();
+		for (Contact x : contacts) {
+			if (x.getName().equals(name)) {
+				System.out.println("Contact " + x.getName() + " found");
+				return contacts.indexOf(x);
+			}
+		}
+		System.out.println("Contact " + name + " not found");
+		return -1;
+
+	}
+	
+	private void searchForContact() {
+		int position = findContact();
+			if (position >= 0) {
+				Contact c = contacts.get(position);
+				System.out.println("Phone number: " + c.getNumber());
+			}
 	}
 
 }
